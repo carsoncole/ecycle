dfb243a310b36f798fdec22137fa8fb7c3a2bbac
Carson Cole <carson@grade.us>
2019-04-22 21:37:33 -0700

    added simplecov; factories

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ group :development, :test do
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
   gem 'factory_bot_rails'
+  gem 'simplecov'
 end
 
 group :development do
---
+++ test/factories.rb
@@ -0,0 +1,43 @@
+FactoryBot.define do
+  # factory :user do
+  #   email { "john_doe@example.com" }
+  # end
+
+  # factory :issuer do
+  #   name { "ACME Rubber Company"}
+  # end
+
+  # factory :security do
+  # end
+
+  # factory :stock do
+  #   account
+  # end
+
+  # factory :bond do
+  #   account
+  # end
+
+  # factory :cash do
+  #   account
+  # end
+
+  # factory :account do
+  #   title { "John Doe"}
+  # end
+
+  # factory :security_transaction do
+  #   date { Date.today }
+  #   quantity { 100 }
+  #   price { 10 }
+  #   transaction_type { 'Buy' }
+  #   account
+  #   association :security, factory: :stock
+  # end
+
+  # factory :cash_transaction do
+  #   date { Date.today }
+  #   account
+  #   association :security, factory: :cash
+  # end
+end
