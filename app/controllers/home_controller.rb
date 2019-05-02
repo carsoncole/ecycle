cba91003d4917796581d9840bb382bbc0239c084
Carson Cole <carson.cole@protonmail.com>
2019-05-01 23:05:10 -0700

    fix for missing Setting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @pickup = Pickup.new
-    @pickups_start_at = Setting.last.pickups_start_at
+    @pickups_start_at = Setting.find_or_create_by(id: 1).pickups_start_at
     @pickups_end_at = Setting.last.pickups_end_at
     @dropoffs_start_at = Setting.last.dropoffs_start_at
     @dropoffs_end_at = Setting.last.dropoffs_end_at
