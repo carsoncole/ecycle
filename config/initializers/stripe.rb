6e5e5a8966fa178b0c293c8dfb10ab6b3da8845c
Carson Cole <carson.cole@protonmail.com>
2019-05-12 08:49:41 -0700

    fix environment versions of keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => Rails.application.credentials.stripe_publishable_key,
-  :secret_key      => Rails.application.credentials.stripe_secret_key
+  :publishable_key => Rails.application.credentials.send(Rails.env)[:stripe_publishable_key],
+  :secret_key      => Rails.application.credentials.send(Rails.env)[:stripe_secret_key]
 }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]
