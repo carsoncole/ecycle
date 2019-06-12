0a8adfc030e35e817e349f3dcc2ca07a2e54ddce
Carson Cole <carson.cole@protonmail.com>
2019-06-11 22:52:39 -0700

    added more tests

--- test/controllers/charges_controller_test.rb
+++ test/controllers/charges_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ChargesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index if logged in" do
+    sign_in(create(:user))
+    get charges_url
+    assert_response :success
+  end
+
+  test "should not get index if not logged in" do
+    get charges_url
+    assert_redirected_to sign_in_url
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,14 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  # test "should get index if logged in" do
+  #   sign_in(create(:user))
+  #   get users_url
+  #   assert_response :success
+  # end
+
+  # test "should not get index if not logged in" do
+  #   get users_url
+  #   assert_redirected_to sign_in_url
+  # end
 end
